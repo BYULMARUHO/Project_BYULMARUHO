62276bf31d879e26b4428fc1828aa5da6883b2bd
LeeYuJoung <nania000822@gmail.com>
2025-06-19 23:45:43 +0900

    Customer Order

--- Assets/02. Scripts/Customer/CustomerController.cs
+++ Assets/02. Scripts/Customer/CustomerController.cs
@@ -1,18 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+using Spine;
 using UnityEngine;
+using UnityEngine.AI;
+using Utils.EnumTypes;
 
 public class CustomerController : MonoBehaviour
 {
+    private NavMeshAgent agent;
     private Pathfinder pathFinder;
     private TargetController targetController;
 
+    public CustomerState state;
     private float moveSpeed = 100.0f;
+
+    private float waitTime = 10.0f;
+    private float currentTime = 0.0f;
+
     private bool isOrder = false;
+    private bool isSit = false;
 
     private void Awake()
     {
+        agent = GetComponent<NavMeshAgent>();
         pathFinder = GetComponent<Pathfinder>();
         targetController = GameObject.Find("Line").GetComponent<TargetController>();
     }
@@ -24,22 +32,68 @@ public class CustomerController : MonoBehaviour
 
     public void Init()
     {
-        pathFinder.moveSpeed = moveSpeed;
-        pathFinder.target = targetController.TargetHandler();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+        //pathFinder.moveSpeed = moveSpeed;
+        StandInLine();
     }
 
     private void Update()
     {
-        
+        StateHandler();
+    }
+
+    public void StateHandler()
+    {
+        switch (state)
+        {
+            case CustomerState.Idle:
+                break;
+            case CustomerState.Walk:
+                break;
+            case CustomerState.Wait:
+                break;
+            case CustomerState.Sit:
+                break;
+            case CustomerState.Eat:
+                break;
+            case CustomerState.Drink:
+                break;
+            case CustomerState.ReJoice:
+                break;
+            case CustomerState.Angry:
+                break;
+            case CustomerState.Truth:
+                break;
+        }
+    }
+
+    public void StandInLine()
+    {
+        //pathFinder.target = targetController.TargetHandler();
+        if(targetController.TargetHandler() !=  null)
+        {
+            Transform target = targetController.TargetHandler();
+            agent.SetDestination(target.position);
+        }
     }
 
     public void RequestOrder()
     {
-
+        Debug.Log("::: Order :::");
+        isOrder = true;
     }
 
     public void StartOrderProcess()
     {
 
+    }
+
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.CompareTag("Line"))
+        {
+            RequestOrder();
+        }
     }
 }
