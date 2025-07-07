927a529ba92cc7b48012f746754bfb2fcb7178aa
LeeYuJoung <nania000822@gmail.com>
2025-07-08 00:06:25 +0900

    Inventory

---
+++ Assets/02. Scripts/Inventory/Slot.cs
@@ -0,0 +1,12 @@
+using TMPro;
+using UnityEngine;
+
+public class Slot : MonoBehaviour
+{
+    public Item item;
+    public int itemCount;
+    public Sprite itemImage;
+
+    private TextMeshProUGUI countText;
+    private GameObject countImage;
+}
